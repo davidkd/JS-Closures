0c496a5a1f6cc319d39fb3fe27aeeb07e5edb6fd
David Danielson <David@Davids-MacBook.local>
2016-09-21 13:49:47 -0600

    completed first three problems

--- closures.js
+++ closures.js
@@ -15,11 +15,11 @@ closure over the name variable. Invoke outer saving the return value into
 another variable called 'inner'. */
 
 // Code Here
-
+var inner = outer();
 //Once you do that, invoke inner.
 
 //Code Here
-
+inner();
 
 
 
@@ -48,7 +48,8 @@ Create a makeCall function that when invoked logs 'Calling Jake at 435-215-9248'
 in your console. */
 
   //Code Here
-
+  var makeCall = callFriend();
+  console.log(makeCall('435-215-9248'));
 
 
 
@@ -67,13 +68,19 @@ in your console. */
 properly. */
 
 //Code Here
-
+var makeCounter = function(){
+  var number = 0;
+  function addOne(){
+  	return number += 1;
+  }
+  return addOne;
+};
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
